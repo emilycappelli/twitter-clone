9c57de7cfc871fc4320d6309861fb5666acd9658
Emily Cappelli <cappelli.emily@gmail.com>
2018-08-20 19:12:54 -0400

    create show_user
    create following action
    create unfollowing action

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -3,19 +3,33 @@ class EpicenterController < ApplicationController
   	# initiate empty array
   	@following_tweets = []
   	# iterate through all tweets, find those that your'e following and push them into arrow
-  	Tweet.all.each do |tweet|
-  		if current_user.following.include?(tweet.user.id) || current_user.id == tweet.user.id
-  			@following_tweets.push(tweet)
-  		end
+  	if current_user
+	  	Tweet.all.each do |tweet|
+	  		if current_user.following.include?(tweet.user.id) || current_user.id == tweet.user.id
+	  			@following_tweets.push(tweet)
+	  		end
+	  	end
   	end
   end
 
   def show_user
+  	@user = User.find(params[:id])
   end
 
   def now_following
+  	# add the user id of the user we want to follow to the following array (it's in the users table)
+  	current_user.following.push(params[:id].to_i)
+  	current_user.save
+
+  	# we don't need a new view - just go back to that users page
+  	redirect_to show_user_path(id: params[:id])
   end
 
   def unfollow
+  	# remove the user id of the user we want to unfollow to the .following array)
+  	current_user.following.delete(params[:id].to_i)
+  	current_user.save
+
+  	redirect_to show_user_path(id: params[:id])
   end
 end
